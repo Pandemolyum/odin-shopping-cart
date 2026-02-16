4f51ea4ed6ec3fc509c3f268a1d8baf23ac8fd37
Pandemolyum <77362203+Pandemolyum@users.noreply.github.com>
2026-02-16 16:04:38 -0500

    Add item quantity button functions

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,15 @@ function App() {
         <>
             <Header />
             <h1>Home Page</h1>
+            <p>Welcome to the shop!</p>
+            <p>
+                Please take a look at the <a href="shop">Shop</a> page and add
+                items to your cart.
+            </p>
+            <p>
+                If you are a compulsive shopper this is the perfect place toi
+                manage your impulses as you cannot checkout your items.
+            </p>
         </>
     );
 }
--- src/Item.tsx
+++ src/Item.tsx
@@ -1,3 +1,5 @@
+import React, { useState } from "react";
+
 export default function Item({
     name,
     imgUrl,
@@ -9,15 +11,47 @@ export default function Item({
     price: number;
     quantity?: number;
 }) {
+    const [qty, setQty] = useState<number>(quantity);
+
+    const handleIncrement = () => {
+        if (qty < 100) {
+            setQty(qty + 1);
+        }
+    };
+
+    const handleDecrement = () => {
+        if (qty > 0) {
+            setQty(qty - 1);
+        }
+    };
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        if (e.target) {
+            const num: number = Number(e.target.value);
+            if (num >= 0 && num <= 100) {
+                setQty(num);
+            }
+        }
+    };
+
     return (
         <div className="item">
             <h3>{name}</h3>
             <img src={imgUrl} alt="" />
             <p>{price}</p>
             <div className="qty">
-                <button className="qty">-</button>
-                <input type="number" className="qty" value={quantity} />
-                <button className="qty">+</button>
+                <button className="qty" onClick={handleDecrement}>
+                    -
+                </button>
+                <input
+                    type="number"
+                    className="qty"
+                    value={qty}
+                    onChange={handleChange}
+                />
+                <button className="qty" onClick={handleIncrement}>
+                    +
+                </button>
             </div>
             <button className="qty">Add to Cart</button>
         </div>
